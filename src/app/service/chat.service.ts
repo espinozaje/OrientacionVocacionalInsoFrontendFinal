c39c75834f0e3c6a8ffd2ffb5e3d2c055fd35ab1
Espinozaje <jeisson12aaron@gmail.com>
2024-11-24 15:26:34 -0500

    hablitacion

--- src/app/service/chat.service.ts
+++ src/app/service/chat.service.ts
@@ -19,7 +19,7 @@ export class ChatService {
 
   initConnectionSocket(){
 
-    const url =  '//orientacion-vocacional.vercel.app:8080/api/v1/chat-socket'
+    const url =  '//orientacionvocacionalapinsofinal-inso.up.railway.app/api/v1/chat-socket'
     const socket = new SockJS(url);
     this.stompClient = Stomp.over(socket);
   }
